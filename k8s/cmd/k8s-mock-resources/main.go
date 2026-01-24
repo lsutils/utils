6af0e1a477d1dad4145ca8972d9c57d6f9eb52d0
acejilam <acejilam@gmail.com>
2026-01-24 17:23:01 +0800

    -
    Signed-off-by: acejilam <acejilam@gmail.com>

--- k8s/cmd/k8s-mock-resources/main.go
+++ k8s/cmd/k8s-mock-resources/main.go
@@ -20,8 +20,8 @@ func main() {
 	node, err := client.CoreV1().Nodes().Get(context.Background(), "koord-worker3", metav1.GetOptions{})
 	fmt.Println(err)
 	node.Labels["dc.com/node.role"] = "worker"
-	node.Status.Capacity["nvidia.com/l40s"] = resource.MustParse("8")
-	node.Status.Allocatable["nvidia.com/l40s"] = resource.MustParse("8")
+	node.Status.Capacity["nvidia.com/l40s"] = resource.MustParse("2")
+	node.Status.Allocatable["nvidia.com/l40s"] = resource.MustParse("2")
 	_, err = client.CoreV1().Nodes().UpdateStatus(context.Background(), node, metav1.UpdateOptions{})
 	fmt.Println(err)
 }
